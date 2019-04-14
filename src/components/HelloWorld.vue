192cd943d98ffaff8417554b63acd767073817be
hanjunpeng <haihjpxiaodan@yeah.net>
2019-04-14 21:03:35 +0800

    commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="testvue">你好</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../assets/css/mixin.scss";
+.testvue {
+  display: flex;
+  width: pxToRem(10px);
+  height: pxToRem(20px);
+  background: #333;
+}
+</style>
