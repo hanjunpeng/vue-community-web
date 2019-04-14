5c5bf30c0f5a65d1369fd8f807c3df8f1ba26aa1
hanjunpeng <1358271059@qq.com>
2019-04-14 22:04:52 +0800

    commit
    提交代码

---
+++ src/components/Base/BaseFooter.vue
@@ -0,0 +1,53 @@
+<template>
+  <ul class="nav-bar-wrap">
+    <li v-for="(item, index) in menuArr" :key="index">
+      {{ item.name }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuArr: [
+        {
+          name: "首页",
+          value: "home"
+        },
+        {
+          name: "全部分类",
+          value: "classify"
+        },
+        {
+          name: "购物车",
+          value: "cart"
+        },
+        {
+          name: "我的",
+          value: "my"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.nav-bar-wrap{
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  justify-content: space-between;
+  display: flex;
+  li{
+    width: 25%;
+    text-align: center;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
+
---
+++ src/components/Base/BaseWrap.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="base-wrap">
+    <router-view></router-view>
+    <BaseFooter class="outer-menu"></BaseFooter>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import BaseFooter from "./BaseFooter.vue";
+export default {
+  components: {
+    BaseFooter
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/css/mixin.scss';
+.outer-menu{
+  border-top: 1px solid #f5f5f5;
+  height: pxToRem(100px);
+}
+</style>
+
